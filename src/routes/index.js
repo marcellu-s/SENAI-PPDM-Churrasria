702c66850426a8c9eb032ed9362bc40fa12558fe
marcellu-s <marcellusfran@outlook.com>
2023-09-16 14:43:51 -0300

    Inicializando projeto

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import Home from '../pages/Home';
+
+// const Stack = createNativeStackNavigator();
+
+// const Routes = () => {
+
+//     return (
+//         <NavigationContainer>
+//             <Stack.Navigator initialRouteName='Home' screenOptions={{ headerShown: false }}>
+//                 <Stack.Screen name="Home" component={Home} />
+//             </Stack.Navigator>
+//         </NavigationContainer>
+//     );
+// };
+
+// export default Routes;
